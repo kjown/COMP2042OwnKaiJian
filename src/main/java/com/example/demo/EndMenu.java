4a3c3acf5e77929797efd24da3a1a0c55afe4c61
kjown <ragingtuna58@gmail.com>
2024-11-04 14:18:10 +0800

    created End Menu class

---
+++ src/main/java/com/example/demo/EndMenu.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import com.example.demo.controller.Controller;
+
+public class EndMenu {
+    private final Stage stage
+}
