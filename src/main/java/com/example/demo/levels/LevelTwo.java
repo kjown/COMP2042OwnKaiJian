9462ae4cdcec2ba11ce8db6e7b0e02459b2fc68a
kjown <ragingtuna58@gmail.com>
2024-11-13 12:11:03 +0800

    feat: implemented LevelTwo with Helicopter enemies with different movement patterns

--- src/main/java/com/example/demo/levels/LevelTwo.java
+++ src/main/java/com/example/demo/levels/LevelTwo.java
@@ -2,18 +2,17 @@ package com.example.demo.levels;
 
 import com.example.demo.actors.ActiveActorDestructible;
 import com.example.demo.actors.Enemy1;
+import com.example.demo.actors.Enemy2;
 import com.example.demo.controller.Controller;
 import com.example.demo.view.LevelView;
 import javafx.stage.Stage;
 
-
-
 public class LevelTwo extends LevelParent {
-    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
+    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.png";
     private static final String NEXT_LEVEL = "com.example.demo.levels.LevelBoss";
-    private static final int TOTAL_ENEMIES = 10;
-    private static final int KILLS_TO_ADVANCE = 20;
-    private static final double ENEMY_SPAWN_PROBABILITY = .50;
+    private static final int TOTAL_ENEMIES = 5;
+    private static final int KILLS_TO_ADVANCE = 5;
+    private static final double ENEMY_SPAWN_PROBABILITY = .2;
     private static final int PLAYER_INITIAL_HEALTH = 5;
 
     public LevelTwo(double screenHeight, double screenWidth, Controller controller, Stage stage) {
@@ -40,12 +39,12 @@ public class LevelTwo extends LevelParent {
         for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
             if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
                 double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
-                ActiveActorDestructible newEnemy = new Enemy1(getScreenWidth(), newEnemyInitialYPosition);
+                ActiveActorDestructible newEnemy = new Enemy2(getScreenWidth(), newEnemyInitialYPosition);
                 addEnemyUnit(newEnemy);
             }
         }
     }
-
+    @Override
     protected LevelView instantiateLevelView() {
         return new LevelView(getRoot(), PLAYER_INITIAL_HEALTH);
     }
--- src/main/java/com/example/demo/view/LevelViewLevelTwo.java
+++ src/main/java/com/example/demo/view/LevelViewLevelTwo.java
@@ -1,4 +1,58 @@
 package com.example.demo.view;
 
+import com.example.demo.PauseMenu;
+import javafx.scene.Group;
+
 public class LevelViewLevelTwo {
+    private static final double HEART_DISPLAY_X_POSITION = 5;
+    private static final double HEART_DISPLAY_Y_POSITION = 25;
+    private static final int WIN_IMAGE_X_POSITION = 355;
+    private static final int WIN_IMAGE_Y_POSITION = 175;
+    private static final int LOSS_SCREEN_X_POSITION = -160;
+    private static final int LOSS_SCREEN_Y_POSISITION = -375;
+    private static final int IMAGE_X_POSITION = 0;
+    private static final int IMAGE_Y_POSITION = 0;
+    private static final int SCREEN_WIDTH = 1300;
+    private static final int SCREEN_HEIGHT = 750;
+    private final Group root;
+    private final WinImage winImage;
+    private final GameOverImage gameOverImage;
+    private final HeartDisplay heartDisplay;
+    private final PauseMenu pauseMenu;
+
+    public LevelViewLevelTwo(Group root, int heartsToDisplay) {
+        this.root = root;
+        this.heartDisplay = new HeartDisplay(HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay);
+        this.winImage = new WinImage(WIN_IMAGE_X_POSITION, WIN_IMAGE_Y_POSITION);
+        this.gameOverImage = new GameOverImage(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSISITION);
+        this.pauseMenu = new PauseMenu(IMAGE_X_POSITION, IMAGE_Y_POSITION);
+    }
+    public void showHeartDisplay() {
+        root.getChildren().add(heartDisplay.getContainer());
+    }
+
+    public void showWinImage() {
+        root.getChildren().add(winImage);
+        winImage.showWinImage();
+    }
+
+
+    public void removeHearts(int heartsRemaining) {
+        int currentNumberOfHearts = heartDisplay.getContainer().getChildren().size();
+        for (int i = 0; i < currentNumberOfHearts - heartsRemaining; i++) {
+            heartDisplay.removeHeart();
+        }
+    }
+    public void showPauseMenuImage() {
+        if (!root.getChildren().contains(pauseMenu)) {
+            root.getChildren().add(pauseMenu);
+        } else {
+            root.getChildren().remove(pauseMenu);
+            root.getChildren().add(pauseMenu);
+        }
+        pauseMenu.showPauseMenu();
+    }
+    public  void hidePauseMenuImage() {
+        pauseMenu.hidePauseMenu();
+    }
 }
