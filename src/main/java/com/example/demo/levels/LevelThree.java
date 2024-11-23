4139caceedb7daa47466541a09b467ed0c9943cc
kjown <ragingtuna58@gmail.com>
2024-11-23 17:57:02 +0800

    fix: change NEXT_LEVEL to LevelFour

--- src/main/java/com/example/demo/levels/LevelThree.java
+++ src/main/java/com/example/demo/levels/LevelThree.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class LevelThree extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background3.jpg";
-    private static final String NEXT_LEVEL = "com.example.demo.levels.LevelBoss";
+    private static final String NEXT_LEVEL = "com.example.demo.levels.LevelFour";
     private static final int PLAYER_INITIAL_HEALTH = 5;
     private static final int TOTAL_ENEMIES = 2;
     private static final int KILLS_TO_ADVANCE = 5;
