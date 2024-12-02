02031aa4cda545948585acc05036c40bc5995f65
kjown <ragingtuna58@gmail.com>
2024-12-02 19:14:21 +0800

    fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,9 @@ module com.example.demo {
     requires javafx.fxml;
     requires javafx.media;
 
-//    opens com.example.demo to javafx.fxml; // Open the main package
-    exports com.example.demo.controller;    // Export the controller package
+    exports com.example.demo.controller;
     opens com.example.demo.actors to javafx.fxml;
     opens com.example.demo.levels to javafx.fxml;
     opens com.example.demo.view to javafx.fxml;
     opens com.example.demo.menu to javafx.fxml;
-    opens com.example.demo.actors.projectiles to javafx.fxml;
-    opens com.example.demo.actors.enemies to javafx.fxml;
-//    opens com.example.demo.menu to javafx.fxml;
 }
